5f873ac004541d4cbbe2690f4c98f44599f14140
Felix Kiprotich <felixkiprotich2000@gmail.com>
2025-04-25 12:31:35 +0300

    better responsive design for the lottie animation effect

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,60 +4,89 @@ import { FaUserCheck } from "react-icons/fa";
 import { MdOutlineEmail } from "react-icons/md";
 import { TbLockPassword } from "react-icons/tb";
 
-
 const Home = () => {
-
   return (
-    <div className='flex flex-col items-center justify-center h-full lg:h-screen w-full lg:grid lg:grid-cols-2 lg:gap-5'>
-       <div className='flex w-full  lg:ml-[100px]  flex-col items-center h-full'>
-            <div className='flex justify-center lg:justify-start w-full  mt-[40px] items-center'>
-                <img src='/betterlogo.png' alt='logo' className='w-[30px] h-auto'/>
-                <span className='text-[13px] ml-2 font-bold'>Health<span className='text-gray-400'>Wise</span></span>
-            </div> 
-            <div className='flex w-full gap-2 items-center mt-[30px] justify-center lg:justify-start flex-col'>
-                <h1 className='flex w-full justify-center lg:justify-start items-center lg:text-[18px] text-[23px] font-bold'>Hello there,.....</h1>
-                <p  className='flex lg:text-[10px] text-[13px] justify-center lg:justify-start text-gray-400 w-full items-center'>Sign in to get access to your account</p>
+    <div className='w-full max-w-screen overflow-x-hidden'>
+      <div className='flex flex-col items-center justify-center min-h-screen w-full lg:grid lg:grid-cols-2 lg:gap-5'>
+        
+        {/* Left Section */}
+        <div className='flex w-full px-4 flex-col items-center lg:items-start lg:ml-[100px] h-full'>
+          
+          {/* Logo */}
+          <div className='flex justify-center lg:justify-start w-full mt-[40px] items-center'>
+            <img src='/betterlogo.png' alt='logo' className='w-[30px] h-auto' />
+            <span className='text-[13px] ml-2 font-bold'>Health<span className='text-gray-400'>Wise</span></span>
+          </div>
+
+          {/* Greeting */}
+          <div className='flex w-full flex-col gap-2 items-center mt-[30px] lg:items-start'>
+            <h1 className='text-[23px] lg:text-[18px] font-bold text-center lg:text-left w-full'>Hello there,.....</h1>
+            <p className='text-[13px] lg:text-[10px] text-gray-400 text-center lg:text-left w-full'>
+              Sign in to get access to your account
+            </p>
+          </div>
+
+          {/* Form */}
+          <form className='flex w-full max-w-[350px] flex-col gap-4 mt-[30px]'>
+            
+            {/* Email */}
+            <label htmlFor='email' className='flex flex-col w-full'>
+              <span className='mb-2 text-[12px] lg:text-[9px] text-gray-400'>Your email address</span>
+              <div className='bg-zinc-800 p-2.5 text-[11px] flex items-center rounded-sm'>
+                <MdOutlineEmail className='text-gray-400' size={15} />
+                <input type='text' className='ml-2 outline-none bg-transparent text-white w-full' placeholder='example@gmail.com' />
+              </div>
+            </label>
+
+            {/* Username */}
+            <label htmlFor='username' className='flex flex-col w-full'>
+              <span className='mb-2 text-[12px] lg:text-[9px] text-gray-400'>Enter your username</span>
+              <div className='bg-zinc-800 p-2.5 text-[11px] flex items-center rounded-sm'>
+                <FaUserCheck className='text-gray-400' size={15} />
+                <input type='text' className='ml-2 outline-none bg-transparent text-white w-full' placeholder='eg Felix Kiprotich' />
+              </div>
+            </label>
+
+            {/* Password */}
+            <label htmlFor='password' className='flex flex-col w-full'>
+              <span className='mb-2 text-[12px] lg:text-[9px] text-gray-400'>Enter your password</span>
+              <div className='bg-zinc-800 p-2.5 text-[11px] flex items-center rounded-sm'>
+                <TbLockPassword className='text-gray-400' size={15} />
+                <input type='password' className='ml-2 outline-none bg-transparent text-white w-full' placeholder='..................' />
+              </div>
+            </label>
+
+            {/* Button */}
+            <div className='w-full mt-4'>
+              <button className='bg-emerald-400 font-bold text-[11px] p-2.5 w-full rounded-sm'>
+                Sign in ......
+              </button>
             </div>
-        <form className='flex w-full gap-4 items-center mt-[30px] justify-center lg:justify-start flex-col'>
-            <label htmlFor='username' className='flex  w-full   flex-col'>
-                <span className='ml-3 lg:ml-0 mb-2 text-[12px] lg:text-[9px] text-gray-400'>Your email address</span>
-                <div className='bg-zinc-800 items-center  lg:w-[350px] lg:p-2 text-[11px] p-2.5 flex lg:-mx-0 mx-3 rounded-sm '>
-                <MdOutlineEmail className='text-gray-400' size={15}/>
-                <input type='text ' className='ml-2 outline-none' placeholder='example@gmail.com'  />
-                </div>
-            </label>
+          </form>
 
-            <label htmlFor='username' className='flex  w-full   flex-col'>
-                <span className='ml-3 lg:ml-0 mb-2 text-[12px] lg:text-[9px]  text-gray-400'>Enter your username</span>
-                <div className='bg-zinc-800 items-center  lg:w-[350px] lg:p-2 text-[11px] p-2.5 flex lg:-mx-0 mx-3 rounded-sm '>
-                <FaUserCheck className='text-gray-400' size={15}/>
-                <input type='text ' className='ml-2 outline-none' placeholder='eg Felix Kiprotich'  />
-                </div>
-            </label>
-            <label htmlFor='username' className='flex  w-full   flex-col'>
-                <span className='ml-3 lg:ml-0 mb-2 text-[12px] lg:text-[9px] text-gray-400'>Enter your password</span>
-                <div className='bg-zinc-800 items-center  lg:w-[350px] text-[11px] p-2.5 lg:p-2 flex  lg:-mx-0 mx-3 rounded-sm '>
-                <TbLockPassword className='text-gray-400' size={15}/>
-                <input type='text ' className='ml-2 outline-none' placeholder='..................'  />
-                </div>
-            </label>
-            <div className='flex  w-full   flex-col mt-4'>
-            <button className='bg-emerald-400 flex justify-center font-bold items-center w-[335px]  lg:w-[350px] mx-3 text-[11px] p-2.5 lg:p-2 flex lg:-mx-0  rounded-sm '>Sign in ......</button>
-            </div>
-        </form>
-
-        <div className='flex w-full lg:justify-start justify-center items-center mt-[60px]'><p className='text-[9px] text-gray-400'>&copy; {new Date().getFullYear()} Your Company Name. All rights reserved.</p></div>
-
+          {/* Copyright */}
+          <div className='flex w-full justify-center lg:justify-start items-center mt-[60px]'>
+            <p className='text-[9px] hidden lg:flex text-gray-400'>
+              &copy; {new Date().getFullYear()} Your Company Name. All rights reserved.
+            </p>
+          </div>
         </div>
-        <div className='flex rounded-lg bg-indigo-50 h-full lg:h-screen justify-center flex-col items-center'>
-        <DotLottieReact
-      src="https://lottie.host/00a8ab11-c159-49c4-b6c5-0bd614de820f/omqsv0Wi5B.lottie"
-      loop
-      autoplay
-      style={{ width: "900px", height: "auto", objectFit: "cover" }}
-    />
-    </div>
-      
+
+        {/* Right Section */}
+        <div className='flex flex-col rounded-lg  lg:mt-0 bg-indigo-50 h-full lg:h-screen justify-center items-center p-4'>
+          <DotLottieReact
+          className='w-100% lg:w-[850px] max-w-500px h-auto object-cover'
+            src="https://lottie.host/00a8ab11-c159-49c4-b6c5-0bd614de820f/omqsv0Wi5B.lottie"
+            loop
+            autoplay
+            // style={{ width: "100%", maxWidth: "500px", height: "auto", objectFit: "cover" }}
+          />
+           <p className='text-[9px] lg:hidden flex text-zinc-900'>
+              &copy; {new Date().getFullYear()} Your Company Name. All rights reserved.
+            </p>
+        </div>
+
+      </div>
     </div>
   )
 }
